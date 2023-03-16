aec8bdd96caf0a5af5df2ebd08f4e158134ddf3e
Dorinetk <dketchiozo@gmail.com>
2023-03-15 22:16:26 -0400

    updated with recent resources

--- s3.tf
+++ s3.tf
@@ -7,19 +7,27 @@ resource "aws_kms_key" "backend-key" {
 # creates s3 bucket
 resource "aws_s3_bucket" "backend-s3" {
   bucket = "bootcamp30-032023-nicole"
+}
+
+resource "aws_s3_bucket_acl" "backend-s3-acl" {
+  bucket = aws_s3_bucket.backend-s3.id
   acl    = "private"
+}
 
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.backend-key.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
+resource "aws_s3_bucket_versioning" "backend-s3-versioning" {
+  bucket = aws_s3_bucket.backend-s3.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend-key-config" {
+  bucket = aws_s3_bucket.backend-s3.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.backend-key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
