d30ef942bda630f41bde230a61988ccdcafd1f68
Dorinetk <dketchiozo@gmail.com>
2023-03-18 19:25:26 -0400

    added random integer

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+# generate a random number to have a unique bucket
+resource "random_integer" "random" {
+  min = 3
+  max = 6
+}
 
+# Creates a bucket using S3 m o dule
 module "s3_bucket" {
     source = "./modules/s3/"
     
-    bucket_name = "bootcamp30-032023-nicole"
+    bucket_name = "bootcamp30-${random_integer.random.id}-nicole"
 
     tags = {
     Terraform   = "true"
